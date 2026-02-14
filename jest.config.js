b0ab88458e17aaae7350f99f8635586d666d77e1
Manas Behera <60974245+manaskumarbehera@users.noreply.github.com>
2026-02-14 09:38:18 +0100

    Refactor jest.config.js to use module.exports syntax for consistency

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
-{
-  "testEnvironment": "node",
-  "testMatch": ["**/__tests__/**/*.test.js", "**/*.test.js"],
-  "collectCoverageFrom": [
-    "test/**/*.js",
-    "!test/**/*.test.js",
-    "!node_modules/**"
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js', '**/*.test.js'],
+  collectCoverageFrom: [
+    'test/**/*.js',
+    '!test/**/*.test.js',
+    '!node_modules/**'
   ],
-  "testTimeout": 30000,
-  "verbose": true,
-  "coverageDirectory": "coverage",
-  "collectCoverage": false
-}
+  testTimeout: 30000,
+  verbose: true,
+  coverageDirectory: 'coverage',
+  collectCoverage: false
+};
 
